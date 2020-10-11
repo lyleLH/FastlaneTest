d52ddef0aaebf0648e8ade1f5527aa4bbe397b7d
Tom <v2top1lyle@gmail.com>
2020-10-11 17:10:30 +0800

    Fastfile Gemfile 测试

---
+++ Pods/Target Support Files/Pods-FastlaneTest-FastlaneTestUITests/Pods-FastlaneTest-FastlaneTestUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_FastlaneTest_FastlaneTestUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FastlaneTest_FastlaneTestUITestsVersionString[];
+
